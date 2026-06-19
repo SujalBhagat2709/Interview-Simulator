13ae131e81f9b93b26b0a4cba6b4c596e4ab16bf
Sujal Bhagat <167618538+SujalBhagat2709@users.noreply.github.com>
2026-06-19 09:31:04 +0530

    Add files via upload

---
+++ simulator.py
@@ -0,0 +1,27 @@
+from interview_engine import (
+    InterviewEngine
+)
+
+print(
+    "\n========================="
+)
+
+print(
+    "INTERVIEW SIMULATOR"
+)
+
+print(
+    "========================="
+)
+
+print(
+    "\nRole:"
+)
+
+print(
+    "Python Backend Developer"
+)
+
+engine = InterviewEngine()
+
+engine.start()
